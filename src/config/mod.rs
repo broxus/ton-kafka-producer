a1864d930031810b938d2019c788865fa4295acd
Ivan Kalinin <i.kalinin@dexpa.io>
2021-10-05 20:33:23 +0300

    Add node stub

---
+++ src/config/mod.rs
@@ -0,0 +1,144 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+
+use self::temp_keys::*;
+
+mod temp_keys;
+
+/// Main application config (full). Used to run relay
+#[derive(Serialize, Deserialize)]
+pub struct AppConfig {
+    /// TON node settings
+    #[serde(default)]
+    pub node_settings: NodeConfig,
+
+    /// log4rs settings.
+    /// See [docs](https://docs.rs/log4rs/1.0.0/log4rs/) for more details
+    #[serde(default = "default_logger_settings")]
+    pub logger_settings: serde_yaml::Value,
+}
+
+/// TON node settings
+#[derive(Serialize, Deserialize)]
+#[serde(default, deny_unknown_fields)]
+pub struct NodeConfig {
+    /// Node public ip. Automatically determines if None
+    pub adnl_public_ip: Option<Ipv4Addr>,
+
+    /// Node port. Default: 30303
+    pub adnl_port: u16,
+
+    /// Path to the DB directory. Default: `./db`
+    pub db_path: PathBuf,
+
+    /// Path to the ADNL keys. Default: `./adnl-keys.json`.
+    /// NOTE: generates new keys if specified path doesn't exist
+    pub temp_keys_path: PathBuf,
+
+    /// Allowed DB size in bytes. Default: one third of all machine RAM
+    pub max_db_memory_usage: usize,
+
+    /// Archives map queue. Default: 16
+    pub parallel_archive_downloads: u32,
+}
+
+impl NodeConfig {
+    pub async fn build_indexer_config(self) -> Result<ton_indexer::NodeConfig> {
+        // Determine public ip
+        let ip_address = match self.adnl_public_ip {
+            Some(address) => address,
+            None => public_ip::addr_v4()
+                .await
+                .ok_or(ConfigError::PublicIpNotFound)?,
+        };
+        log::info!("Using public ip: {}", ip_address);
+
+        // Generate temp keys
+        // TODO: add param to generate new temp keys
+        let temp_keys =
+            TempKeys::load(self.temp_keys_path, false).context("Failed to load temp keys")?;
+
+        // Prepare DB folder
+        std::fs::create_dir_all(&self.db_path)?;
+
+        // Done
+        Ok(ton_indexer::NodeConfig {
+            ip_address: SocketAddrV4::new(ip_address, self.adnl_port),
+            adnl_keys: temp_keys.into(),
+            rocks_db_path: self.db_path.join("rocksdb"),
+            file_db_path: self.db_path.join("files"),
+            // NOTE: State GC is disabled until it is fully tested
+            state_gc_options: None,
+            old_blocks_policy: Default::default(),
+            shard_state_cache_enabled: false,
+            max_db_memory_usage: self.max_db_memory_usage,
+            parallel_archive_downloads: self.parallel_archive_downloads,
+            adnl_options: Default::default(),
+            rldp_options: Default::default(),
+            dht_options: Default::default(),
+            neighbours_options: Default::default(),
+            overlay_shard_options: Default::default(),
+        })
+    }
+}
+
+impl Default for NodeConfig {
+    fn default() -> Self {
+        Self {
+            adnl_public_ip: None,
+            adnl_port: 30303,
+            db_path: "db".into(),
+            temp_keys_path: "adnl-keys.json".into(),
+            max_db_memory_usage: ton_indexer::default_max_db_memory_usage(),
+            parallel_archive_downloads: 16,
+        }
+    }
+}
+
+impl ConfigExt for ton_indexer::GlobalConfig {
+    fn from_file<P>(path: &P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let file = std::fs::File::open(path)?;
+        let reader = std::io::BufReader::new(file);
+        let config = serde_json::from_reader(reader)?;
+        Ok(config)
+    }
+}
+
+pub trait ConfigExt: Sized {
+    fn from_file<P>(path: &P) -> Result<Self>
+    where
+        P: AsRef<Path>;
+}
+
+fn default_logger_settings() -> serde_yaml::Value {
+    const DEFAULT_LOG4RS_SETTINGS: &str = r##"
+    appenders:
+      stdout:
+        kind: console
+        encoder:
+          pattern: "{d(%Y-%m-%d %H:%M:%S %Z)(utc)} - {h({l})} {M} = {m} {n}"
+    root:
+      level: error
+      appenders:
+        - stdout
+    loggers:
+      ton_kafka_producer:
+        level: info
+        appenders:
+          - stdout
+        additive: false
+    "##;
+    serde_yaml::from_str(DEFAULT_LOG4RS_SETTINGS).unwrap()
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ConfigError {
+    #[error("Failed to find public ip")]
+    PublicIpNotFound,
+}
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use anyhow::{Context, Result};
+use argh::FromArgs;
+use serde::Deserialize;
+use ton_kafka_producer::config::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    run(argh::from_env()).await
+}
+
+async fn run(app: App) -> Result<()> {
+    let config: AppConfig = read_config(app.config)?;
+    let _global_config = ton_indexer::GlobalConfig::from_file(&app.global_config)
+        .context("Failed to open global config")?;
+
+    init_logger(&config.logger_settings).context("Failed to init logger")?;
+
+    log::info!("Initializing producer...");
+
+    // TODO
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, FromArgs)]
+#[argh(description = "A simple service to stream TON data into Kafka")]
+struct App {
+    /// path to config file ('config.yaml' by default)
+    #[argh(option, short = 'c', default = "String::from(\"config.yaml\")")]
+    config: String,
+
+    /// path to global config file
+    #[argh(option, short = 'g')]
+    global_config: String,
+}
+
+fn read_config<P, T>(path: P) -> Result<T>
+where
+    P: AsRef<std::path::Path>,
+    for<'de> T: Deserialize<'de>,
+{
+    let data = std::fs::read_to_string(path).context("Failed to read config")?;
+    let re = regex::Regex::new(r"\$\{([a-zA-Z_][0-9a-zA-Z_]*)\}").unwrap();
+    let result = re.replace_all(&data, |caps: &regex::Captures| {
+        match std::env::var(&caps[1]) {
+            Ok(value) => value,
+            Err(_) => {
+                log::warn!("Environment variable {} was not set", &caps[1]);
+                String::default()
+            }
+        }
+    });
+
+    let mut config = config::Config::new();
+    config.merge(config::File::from_str(
+        result.as_ref(),
+        config::FileFormat::Yaml,
+    ))?;
+
+    config.try_into().context("Failed to parse config")
+}
+
+fn init_logger(config: &serde_yaml::Value) -> Result<()> {
+    let config = serde_yaml::from_value(config.clone())?;
+    log4rs::config::init_raw_config(config)?;
+    Ok(())
+}
