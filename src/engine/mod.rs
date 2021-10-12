f8db49d537cb547b06dcfe32df6101bf37c76faf
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-10-12 13:44:02 +0300

    Implement rpc

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,7 +2,10 @@ use std::sync::Arc;
 
 use anyhow::{Context, Result};
 use futures::TryStreamExt;
+use nekoton::transport::models::ExistingContract;
+use nekoton_indexer_utils::{contains_account, ExistingContractExt};
 use once_cell::sync::OnceCell;
+use parking_lot::RwLock;
 use tiny_adnl::utils::*;
 use ton_block::{Deserializable, HashmapAugType, Serializable};
 use ton_block_compressor::ZstdWrapper;
@@ -14,7 +17,7 @@ use crate::config::*;
 use self::kafka_producer::*;
 
 mod kafka_producer;
-
+mod rpc;
 pub struct Engine {
     indexer: Arc<ton_indexer::Engine>,
 }
@@ -56,6 +59,7 @@ struct TonSubscriber {
     account_producer: Option<KafkaProducer>,
     block_proof_producer: Option<KafkaProducer>,
     compressor: Option<ZstdWrapper>,
+    shard_accounts_cache: RwLock<FxHashMap<ton_block::ShardIdent, ton_block::ShardAccounts>>,
 }
 
 impl TonSubscriber {
@@ -76,6 +80,7 @@ impl TonSubscriber {
             account_producer: make_producer(config.account_producer)?,
             block_proof_producer: make_producer(config.block_proof_producer)?,
             compressor,
+            shard_accounts_cache: Default::default(),
         }))
     }
 }
@@ -134,6 +139,36 @@ impl TonSubscriber {
         }
 
         futures.try_collect::<Vec<_>>().await?;
+        if !block_stuff.id().is_masterchain() {
+            let mut shard_accounts_cache = self.shard_accounts_cache.write();
+            let block_info = &block_stuff.block().read_info()?;
+            let shard_state = if let Some(a) = shard_state {
+                a
+            } else {
+                return Ok(());
+            };
+            let shard_accounts = shard_state.state().read_accounts()?;
+            shard_accounts_cache.insert(*block_info.shard(), shard_accounts);
+            if block_info.after_merge() || block_info.after_split() {
+                let block_ids = block_info.read_prev_ids()?;
+                match block_ids.len() {
+                    1 => {
+                        let (left, right) = block_ids[0].shard_id.split()?;
+                        if shard_accounts_cache.contains_key(&left)
+                            && shard_accounts_cache.contains_key(&right)
+                        {
+                            shard_accounts_cache.remove(&block_ids[0].shard_id);
+                        }
+                    }
+                    len if len > 1 => {
+                        for block_id in block_info.read_prev_ids()? {
+                            shard_accounts_cache.remove(&block_id.shard_id);
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
 
         Ok(())
     }
@@ -254,6 +289,22 @@ impl TonSubscriber {
 
         // Done
         Ok(records)
+    }
+
+    pub fn get_contract_state(&self, account: &UInt256) -> Result<Option<ExistingContract>> {
+        let shard_accounts_cache = self.shard_accounts_cache.read();
+        for (shard_ident, shard_accounts) in shard_accounts_cache.iter() {
+            if contains_account(shard_ident, account) {
+                match shard_accounts.get(account) {
+                    Ok(account) => return ExistingContract::from_shard_account_opt(&account),
+                    Err(e) => {
+                        log::error!("Failed to get account {}: {:?}", account.to_hex_string(), e);
+                    }
+                };
+            }
+        }
+
+        Ok(None)
     }
 }
 
