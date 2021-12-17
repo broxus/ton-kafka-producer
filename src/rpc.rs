448951b2fe5c098fd959cf0f6a67bf8861f6e944
Ivan Kalinin <i.kalinin@dexpa.io>
2021-12-17 22:36:00 +0300

    Fix StateReceiveRequest

--- src/rpc.rs
+++ src/rpc.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use nekoton_utils::TrustMe;
+use nekoton_utils::*;
 use serde::{Deserialize, Serialize};
 use ton_block::MsgAddressInt;
 use warp::http::StatusCode;
@@ -31,15 +32,32 @@ pub async fn serve(
     warp::serve(routes).bind(addr).await;
 }
 
-#[derive(Serialize, Deserialize)]
-struct StateReceiveRequest {
-    #[serde(default = "default_wc")]
-    wc: i8,
-    account_id: String,
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(untagged)]
+enum StateReceiveRequest {
+    Old {
+        account_id: String,
+    },
+    New {
+        #[serde(with = "serde_address")]
+        address: ton_block::MsgAddressInt,
+    },
 }
 
-fn default_wc() -> i8 {
-    0
+impl TryFrom<StateReceiveRequest> for ton_block::MsgAddressInt {
+    type Error = anyhow::Error;
+
+    fn try_from(value: StateReceiveRequest) -> Result<Self, Self::Error> {
+        match value {
+            StateReceiveRequest::Old { account_id } => {
+                if account_id.len() != 64 {
+                    return Err(anyhow::Error::msg("Invalid account id"));
+                }
+                MsgAddressInt::from_str(&format!("0:{}", account_id))
+            }
+            StateReceiveRequest::New { address } => Ok(address),
+        }
+    }
 }
 
 struct State {
@@ -51,14 +69,8 @@ async fn state_receiver(
     ctx: Arc<State>,
     data: StateReceiveRequest,
 ) -> Result<Box<dyn Reply>, Infallible> {
-    if data.account_id.len() != 32 {
-        return Ok(Box::new(reply::with_status(
-            format!("Expected len 32. Got: {}", data.account_id.len()),
-            StatusCode::BAD_REQUEST,
-        )));
-    }
-    let address = match MsgAddressInt::from_str(&format!("{}:{}", data.wc, data.account_id)) {
-        Ok(a) => a,
+    let address = match ton_block::MsgAddressInt::try_from(data) {
+        Ok(address) => address,
         Err(e) => {
             log::error!("Bad request: {:?}", e);
             return Ok(Box::new(reply::with_status(
@@ -67,6 +79,7 @@ async fn state_receiver(
             )));
         }
     };
+
     log::info!("Got {} request", address);
     ctx.metrics.processed();
 
@@ -94,4 +107,34 @@ where
     for<'a> T: serde::Deserialize<'a> + Send,
 {
     warp::body::content_length_limit(1024).and(warp::filters::body::json::<T>())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn state_receive_request_parse() {
+        let target_address = ton_block::MsgAddressInt::default();
+
+        assert_eq!(
+            serde_json::from_str::<StateReceiveRequest>(
+                "{\"account_id\":\"0000000000000000000000000000000000000000000000000000000000000000\"}",
+            )
+            .unwrap(),
+            StateReceiveRequest::Old {
+                account_id: hex::encode(target_address.address().get_bytestring(0))
+            }
+        );
+
+        assert_eq!(
+            serde_json::from_str::<StateReceiveRequest>(
+                "{\"address\":\"0:0000000000000000000000000000000000000000000000000000000000000000\"}",
+            )
+            .unwrap(),
+            StateReceiveRequest::New {
+                address: target_address
+            }
+        );
+    }
 }
