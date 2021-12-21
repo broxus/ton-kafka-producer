a6a050c26351aff7983fd13496b30b03507ffa81
Ivan Kalinin <i.kalinin@dexpa.io>
2021-12-21 19:22:57 +0300

    Refactor states cache

--- src/network_scanner/shard_accounts_subscriber.rs
+++ src/network_scanner/shard_accounts_subscriber.rs
@@ -13,8 +13,8 @@ pub type FxHashMap<K, V> = HashMap<K, V, BuildHasherDefault<FxHasher>>;
 
 #[derive(Default)]
 pub struct ShardAccountsSubscriber {
+    masterchain_accounts_cache: RwLock<ton_block::ShardAccounts>,
     shard_accounts_cache: RwLock<FxHashMap<ton_block::ShardIdent, ton_block::ShardAccounts>>,
-    masterchain_accounts_cache: RwLock<FxHashMap<ton_block::ShardIdent, ton_block::ShardAccounts>>,
 }
 
 impl ShardAccountsSubscriber {
@@ -31,32 +31,48 @@ impl ShardAccountsSubscriber {
         let block_info = &block_stuff.block().read_info()?;
         let shard_accounts = shard_state.state().read_accounts()?;
 
-        let mut shard_accounts_cache = if block_stuff.id().is_masterchain() {
-            self.masterchain_accounts_cache.write()
+        if block_stuff.id().is_masterchain() {
+            *self.masterchain_accounts_cache.write() = shard_accounts;
         } else {
-            self.shard_accounts_cache.write()
-        };
+            let mut cache = self.shard_accounts_cache.write();
 
-        shard_accounts_cache.insert(*block_info.shard(), shard_accounts);
-        if block_info.after_merge() || block_info.after_split() {
-            let block_ids = block_info.read_prev_ids()?;
-            match block_ids.len() {
-                1 => {
-                    let (left, right) = block_ids[0].shard_id.split()?;
-                    if shard_accounts_cache.contains_key(&left)
-                        && shard_accounts_cache.contains_key(&right)
-                    {
-                        shard_accounts_cache.remove(&block_ids[0].shard_id);
+            cache.insert(*block_info.shard(), shard_accounts);
+            if block_info.after_merge() || block_info.after_split() {
+                log::warn!("Clearing shard states cache after shards merge/split");
+
+                let block_ids = block_info.read_prev_ids()?;
+
+                match block_ids.len() {
+                    // Block after split
+                    //       |
+                    //       *  - block A
+                    //      / \
+                    //     *   *  - blocks B', B"
+                    1 => {
+                        // Find all split shards for the block A
+                        let (left, right) = block_ids[0].shard_id.split()?;
+
+                        // Remove parent shard of the block A
+                        if cache.contains_key(&left) && cache.contains_key(&right) {
+                            cache.remove(&block_ids[0].shard_id);
+                        }
                     }
-                }
-                len if len > 1 => {
-                    for block_id in block_info.read_prev_ids()? {
-                        shard_accounts_cache.remove(&block_id.shard_id);
+
+                    // Block after merge
+                    //     *   *  - blocks A', A"
+                    //      \ /
+                    //       *  - block B
+                    //       |
+                    2 => {
+                        // Find and remove all parent shards
+                        for block_id in block_info.read_prev_ids()? {
+                            cache.remove(&block_id.shard_id);
+                        }
                     }
+                    _ => {}
                 }
-                _ => {}
             }
-        }
+        };
 
         Ok(())
     }
@@ -65,26 +81,22 @@ impl ShardAccountsSubscriber {
         &self,
         account: &ton_block::MsgAddressInt,
     ) -> Result<Option<ExistingContract>> {
-        let shard_accounts_cache = if account.is_masterchain() {
-            self.masterchain_accounts_cache.read()
-        } else {
-            self.shard_accounts_cache.read()
-        };
-
+        let is_masterchain = account.is_masterchain();
         let account = account.address().get_bytestring_on_stack(0);
         let account = ton_types::UInt256::from_slice(account.as_slice());
 
-        for (shard_ident, shard_accounts) in shard_accounts_cache.iter() {
-            if contains_account(shard_ident, &account) {
-                match shard_accounts.get(&account) {
-                    Ok(account) => return ExistingContract::from_shard_account_opt(&account),
-                    Err(e) => {
-                        log::error!("Failed to get account {}: {:?}", account.to_hex_string(), e);
-                    }
-                };
+        if is_masterchain {
+            let cache = self.masterchain_accounts_cache.read();
+            ExistingContract::from_shard_account_opt(&cache.get(&account)?)
+        } else {
+            let cache = self.shard_accounts_cache.read();
+            for (shard_ident, shard_accounts) in cache.iter() {
+                if !contains_account(shard_ident, &account) {
+                    continue;
+                }
+                return ExistingContract::from_shard_account_opt(&shard_accounts.get(&account)?);
             }
+            Ok(None)
         }
-
-        Ok(None)
     }
 }
